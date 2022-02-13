c280e4cac573bb441708dea816cfc24ac98f820f
asakanobuaki <s1531007@gmail.com>
2022-02-13 22:01:49 +0900

    Add 状態変更時のmodal表示実装

--- app/javascript/smoking.js
+++ app/javascript/smoking.js
@@ -15,15 +15,38 @@ $(document).on('turbolinks:load', function() {
         console.log(data);
 
         const state = data.state;
+        const excessCigarette = data.excess_cigarette;
         const todaySmoking = data.user_smoking;
         const remaining = data.remianing_smoking;
 
+        if (excessCigarette == 6){
+          $("#modalBaldness").fadeIn();
+          $('#closeModal , #modalBg').on('click', function(){
+            $('#modalBaldness').fadeOut();
+          });
+        }
+        else if (excessCigarette == 8){
+          $("#modalCancer").fadeIn();
+          $('#closeModal , #modalBg').on('click', function(){
+            $('#modalCancer').fadeOut();
+          });
+        }
+        else if (excessCigarette == 10){
+          $("#modalDead").fadeIn();
+          $('#closeModal , #modalBg').on('click', function(){
+            $('#modalDead').fadeOut();
+          });
+        }
+
         if (state == "baldness"){
+
           $("#js-status").html("<div class='status-text'>ステータス：ハゲ</div>");
+
           $(".nicotti-position, .nicotti-position-2").fadeOut();
             $(".nicotti-position-1").fadeIn();
          }
         else if (state == "cancer") {
+
           $("#js-status").html("<div class='status-text'>ステータス：がん</div>");
           $(".nicotti-position, .nicotti-position-1").fadeOut();
             $(".nicotti-position-2").fadeIn();
