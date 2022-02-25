dfd3c93c16db2d93ab2076813da82b1115269d15
asakanobuaki <s1531007@gmail.com>
2022-02-25 16:32:06 +0900

    Fix skip_before_action :require_login変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :info, :warning, :danger
+
+  private
+
+  def not_authenticated
+    flash[:error] = 'ログインしてください'
+    redirect_to login_path
+  end
+
 end
