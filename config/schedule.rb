dd7d6ebdaf83ddb2071c85387fed7774f3c8d824
asakanobuaki <s1531007@gmail.com>
2022-03-23 16:00:46 +0900

    Add バディのLINEへ喫煙本数通知機能実装 #28

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,23 +1,3 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 require 'active_support'
 
@@ -32,4 +12,3 @@ job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\
 every 1.day, at: Date.today.end_of_day do
   rake 'registration_smokings:save_number_of_smokings'
 end
-
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -6,3 +6,21 @@ task save_number_of_smokings: :environment do
     PerDaySmoking.create!(user_id: user.id, number_of_smoking: yesterday_smokings, smoked_on: Date.yesterday)
   end
 end
+
+desc 'バディに昨日の喫煙本数をLINEで送信'
+task push_message_to_buddy: :environment do
+
+  client ||= Line::Bot::Client.new { |config|
+    config.channel_id = ENV["LINE_CHANNEL_ID"]
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+
+  User.find_each do |user|
+    user_ids = user.buddies.pluck(:line_id)
+    message = user.set_bubble
+    client.multicast(user_ids, message)
+  end
+
+  p '送信完了'
+end
