9cde281d69cbe9f8c589b5387df285d4bb0a341a
asakanobuaki <s1531007@gmail.com>
2022-03-27 21:26:19 +0900

    Add バディ一覧画面とLINE紹介機能

---
+++ app/controllers/buddies_controller.rb
@@ -0,0 +1,13 @@
+class BuddiesController < ApplicationController
+
+
+  def index
+    @buddies = current_user.buddies
+  end
+
+  def destroy
+    @buddy = Buddy.find(params[:id])
+    @buddy.destroy
+    redirect_to buddies_path, success: 'バディの連携を解除しました。'
+  end
+end
