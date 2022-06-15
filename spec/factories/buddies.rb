a39dcccee18c9feaec2c81b6b119bfc4534b09b3
asakanobuaki <s1531007@gmail.com>
2022-06-15 19:04:23 +0900

    [add] buddyモデルスペック追加 #47

--- spec/factories/buddies.rb
+++ spec/factories/buddies.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :buddy do
-    
+    sequence(:line_id) { |n| "U0e1827745#{n}"}
+    sequence(:name) { |n| "バディ#{n}" }
+    buddy_image { 'default.image' }
   end
 end
--- spec/models/buddy_spec.rb
+++ spec/models/buddy_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Buddy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'line_id, name, buddy_imageがあれば有効な状態であること' do
+    buddy = build(:buddy)
+    expect(buddy).to be_valid
+  end
+
+  it 'line_idがなければ無効な状態であること' do
+    buddy = build(:buddy, line_id: nil)
+    expect(buddy).to_not be_valid
+  end
+
+  it '重複したline_idは無効な状態であること' do 
+    other_buddy = create(:buddy)
+    buddy = build(:buddy, line_id: other_buddy.line_id)
+    expect(buddy).to_not be_valid
+  end
 end
