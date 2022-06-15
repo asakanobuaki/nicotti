557363edbe945b8cd3087483214334722c8b1ccb
asakanobuaki <s1531007@gmail.com>
2022-06-15 15:07:22 +0900

    [add] per_day_smokingモデルテスト追加 #47

--- spec/factories/per_day_smokings.rb
+++ spec/factories/per_day_smokings.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :per_day_smoking do
     number_of_smoking { 1 }
-    smoked_on { "2022-01-27" }
-    user { nil }
+    smoked_on { Date.today }
+    association :user
+
+    trait :no_user_id do
+      user { nil }
+    end
   end
 end
