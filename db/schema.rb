4168b65ce24361fe4a1745b49be688153f4c3a8e
asakanobuaki <s1531007@gmail.com>
2022-06-22 15:04:13 +0900

    [add] PerDaySmokingモデル　ユニーク制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_023124) do
+ActiveRecord::Schema.define(version: 2022_06_22_023956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_023124) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["smoked_on", "user_id"], name: "index_per_day_smokings_on_smoked_on_and_user_id", unique: true
     t.index ["user_id"], name: "index_per_day_smokings_on_user_id"
   end
 
