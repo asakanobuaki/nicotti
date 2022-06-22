48b592cf7c33460c380cabe75922115b7cecf3a7
asakanobuaki <s1531007@gmail.com>
2022-06-22 16:50:26 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,25 +27,23 @@ class User < ApplicationRecord
   end
 
   def today_smokings_count
-    smokings.where(created_at: Date.today.all_day).count
+    smokings.where(created_at: Time.zone.today.all_day).count
   end
 
   def yesterday_smokings_count
-    smokings.where(created_at: Date.yesterday.all_day).count
+    smokings.where(created_at: Time.zone.yesterday.all_day).count
   end
 
   def monthly_smokings_count
-    smokings.where(created_at: Date.yesterday.all_month).count
+    smokings.where(created_at: Time.zone.yesterday.all_month).count
   end
 
   # 1日の目標喫煙本数を超過している場合、超過本数が１プラスされる。
   def target_over
-    if today_smokings_count > target_number
-      self.excess_cigarette += 1
-    end
+    return self.excess_cigarette += 1 if today_smokings_count > target_number
   end
 
-    # 超過喫煙本数に応じてステータスを変更
+  # 超過喫煙本数に応じてステータスを変更
   def user_state
     case excess_cigarette
     when 6
@@ -64,23 +62,23 @@ class User < ApplicationRecord
     healthy!
   end
 
-    # 超過喫煙本数に達したらtrueを返す
+  # 超過喫煙本数に達したらtrueを返す
   def reborn?
     excess_cigarette == 10
   end
 
   def set_bubble
     {
-      type: "flex",
-      altText: "喫煙集計の通知です。",
+      type: 'flex',
+      altText: '喫煙集計の通知です。',
       contents: {
-                  type: "bubble",
-                  size: "mega",
-                  hero: set_hero,
-                  body: set_body
-                  # footer: set_footer,
-                  # style: set_style
-                }
+        type: 'bubble',
+        size: 'mega',
+        hero: set_hero,
+        body: set_body
+        # footer: set_footer,
+        # style: set_style
+      }
     }
   end
 
@@ -90,106 +88,105 @@ class User < ApplicationRecord
       url: 'https://lh3.googleusercontent.com/T4UPvBgdGfq6LUsOPXQevIhnruzGlSLXqX15_HTGowRopsMdaF8fW-jb8GNwMmP136OuMrqw74oebresStrf0cysOa3ljAGzvb3eyEdZObqmFdyG3VdQF516xNHgXA1RrGl3AbHAuOWIorj_qceLkxntKxUAuQw5Wfxm6uTlqUwYJ8BevFxfocmz_4l-GOb-pqk9fShHxRlvJiqajyfd3yMBj9h0G2YNa_xbJDJ6-oboO7MEJ9HyEZEL23xxrCdLAhUnHVleO_34BiYsuAUB052KKdR6jzQ9Tr15pnf5ZlNMLzp0Mqsc2goz7wwrpiIWe4jZJB5gK5BK6iPUkM7771d1C-5WL0cKEmCOVlialSyJt8LXWxL1F53eAfQ2Y1CZCRFb2LX3mru9nmGC3KdpmjWpSbqpeRa74l0i7CklasYr6zYnsDPBti_H3tRWAxEomJ2qmzA_SSUbXxxSY3O3Fq5rllm2kJC7vZZaEwhkLG2P-xb2dI2Ro9Rn93mxBnx28em-yeOCKBB3wCJ5ZyCoSUrJsWxYJK0zYjRWVvs119xEGJ8E0kbCW_SB5cx6GADcgSW9uuwXgeVndpyJbnWd469xBz1hXzyRfG1U6QyCaMQtoyW-vFrKK59FOBU_Tl1FdHraqpvJEHcC8FeQdjri9D-2PiaIbMg_O_35Jj6r3bdvNNxuoJbI9gFT3GywxSQCq6aFnT6tTFpOJqXRxjd2mOZ1z71uQ6CC1vaLKiYiQ3elk7KM_rFy8khJSsigYn_zCJr_OPqNCMaPWCYP3jiANd2hbqp1spfTHbh07MvNAOYUTZ8gmlpbjVmjlFdYM1WfDKcjzQjfnKZ4ml9TnaZXjj-Wx-rRcye4b-K6zJ8=w924-h600-no?authuser=2',
       size: 'full',
       aspectRatio: '20:13',
-      aspectMode: 'cover',
+      aspectMode: 'cover'
     }
   end
 
   def set_body
     {
-      type: "box",
-      layout: "vertical",
-      spacing: "md",
+      type: 'box',
+      layout: 'vertical',
+      spacing: 'md',
       contents: [
         {
-          type: "text",
-          text: "#{I18n.l Date.today, format: :long}"
+          type: 'text',
+          text: I18n.l(Time.zone.today, format: :long).to_s
         },
         {
-          type: "text",
+          type: 'text',
           text: "#{name}さんの昨日の喫煙報告",
-          size: "lg",
-          weight: "bold"
+          size: 'lg',
+          weight: 'bold'
         },
         {
-          type: "separator"
+          type: 'separator'
         },
         {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'sm',
           contents: [
             {
-              type: "box",
-              layout: "baseline",
+              type: 'box',
+              layout: 'baseline',
               contents: [
                 {
-                  type: "text",
-                  text: "#{I18n.l Date.yesterday}の喫煙本数",
-                  weight: "bold",
-                  margin: "xs",
+                  type: 'text',
+                  text: "#{I18n.l Time.zone.yesterday}の喫煙本数",
+                  weight: 'bold',
+                  margin: 'xs',
                   flex: 0,
-                  size: "md"
+                  size: 'md'
                 },
                 {
-                  type: "text",
+                  type: 'text',
                   text: "#{yesterday_smokings_count}本",
-                  size: "lg",
-                  align: "end",
-                  weight: "bold"
+                  size: 'lg',
+                  align: 'end',
+                  weight: 'bold'
                 }
               ]
             },
             {
-              type: "box",
-              layout: "baseline",
+              type: 'box',
+              layout: 'baseline',
               contents: [
                 {
-                  type: "text",
-                  text: "1日の目標喫煙本数",
-                  weight: "bold",
-                  margin: "sm",
+                  type: 'text',
+                  text: '1日の目標喫煙本数',
+                  weight: 'bold',
+                  margin: 'sm',
                   flex: 0
                 },
                 {
-                  type: "text",
+                  type: 'text',
                   text: "#{target_number}本",
-                  size: "lg",
-                  align: "end",
-                  weight: "bold"
+                  size: 'lg',
+                  align: 'end',
+                  weight: 'bold'
                 }
               ]
             },
             {
-              type: "box",
-              layout: "baseline",
+              type: 'box',
+              layout: 'baseline',
               contents: [
                 {
-                  type: "text",
-                  text: "#{Date.yesterday.month}月の総喫煙本数",
-                  weight: "bold",
-                  margin: "sm",
+                  type: 'text',
+                  text: "#{Time.zone.yesterday.month}月の総喫煙本数",
+                  weight: 'bold',
+                  margin: 'sm',
                   flex: 0
                 },
                 {
-                  type: "text",
+                  type: 'text',
                   text: "#{monthly_smokings_count}本",
-                  size: "lg",
-                  align: "end",
-                  weight: "bold"
+                  size: 'lg',
+                  align: 'end',
+                  weight: 'bold'
                 }
               ]
             }
           ]
         },
         {
-          type: "text",
+          type: 'text',
           text: "バディコード: #{invite_code}",
           wrap: true,
-          color: "#aaaaaa",
-          size: "xxs"
+          color: '#aaaaaa',
+          size: 'xxs'
         }
       ]
     }
-    end
-
+  end
 end
